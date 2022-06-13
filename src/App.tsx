3678149eb3f0440868bf9c89ef2dc6a58b66eb15
Joker <pic123123@nate.com>
2022-06-13 11:16:42 +0900

    tailwind css 지ㄴ짜완성~

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Hello tailwind</h1>
+    <div className="m-5 bg-yellow-500 text-blue-500 font-bold">
+      배워서 나주는 React에 Tailwind Css 적용하기
     </div>
   );
 }
