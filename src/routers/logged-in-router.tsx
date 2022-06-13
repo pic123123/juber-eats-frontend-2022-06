e68605637086b541d459ee48f2b04e6e4495c724
Joker <pic123123@nate.com>
2022-06-13 11:50:35 +0900

    Reactive variables

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
 function LoggedInRouter() {
-  return <div>logged-in-router</div>;
+  const onClick = () => {
+    isLoggedInVar(false);
+  };
+  return (
+    <>
+      <h1>로그인 라우터 </h1>
+      <button onClick={onClick}> Click to LogOut</button>
+    </>
+  );
 }
 
 export default LoggedInRouter;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
 function LoggedOutRouter() {
+  const onClick = () => {
+    isLoggedInVar(true);
+  };
   return (
     <>
-      <span>logged-Out-router</span>
+      <h1>로그아웃 라우터</h1>
+      <button onClick={onClick}> Click to LogIn</button>
     </>
   );
 }
